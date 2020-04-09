23baebedcefc2d0dcf74b72ea535b05bb097eeea
or_kachlon <or.kachlon@mail.huji.ac.il>
2020-04-09 23:29:07 +0300

    finished writing the parser and parsed the huge file into smaller ones containing only the reviews and scores. added .idea/ to .gitignore

---
+++ data_organizer.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+from Parser import LargeFileParser
+from FileData import *
+
+
+LARGE_INPUT_FILE = 'ml-dataset/movies.txt'
+LARGE_INPUT_FILE_ENCODING = 'iso-8859-1'
+STEP_SIZE = 100000
+
+
+def parse_file(path):
+    remaining = 0
+    file_data = []
+    with open(path, 'r', encoding=LARGE_INPUT_FILE_ENCODING) as f:
+        for i, l in enumerate(f):
+            pass
+    length = i + 1
+
+    remaining = length
+    parser = LargeFileParser(path, LARGE_INPUT_FILE_ENCODING)
+    while remaining > 0:
+        file_data.append(FileData(parser, STEP_SIZE))
+        remaining = length - parser.get_line_cursor()
+
+    for i, data in enumerate(file_data):
+        data.write_to_file('out/{0}_{1}.txt'.format(path.split('.')[0].split('/')[1], i + 1))
+
+
+def main():
+    parse_file(LARGE_INPUT_FILE)
+
+
+if __name__ == '__main__':
+    main()
