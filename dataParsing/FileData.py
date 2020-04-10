c537f63398312ce1b891b4feae657993e6ae7a10
or_kachlon <or.kachlon@mail.huji.ac.il>
2020-04-10 12:18:27 +0300

    Converted writing format to csv

--- dataParsing/FileData.py
+++ dataParsing/FileData.py
@@ -2,12 +2,13 @@ class FileData:
     """
     A container for ReviewData objects. Can read reviews from file or use parser to parse the original file
     """
-    def __init__(self, step_size: int, parser=None, path=None):
+    def __init__(self, step_size=None, parser=None, path=None, encoding=None):
         self.__reviews = []
         self.__parser = parser
-        if path is not None:
+        if path is not None and encoding is not None:
+            self.__encoding = encoding
             self.__read_from_file(path)
-        elif parser is not None:
+        elif parser is not None and step_size is not None:
             i = 0
             while parser.can_parse() and i < step_size:
                 self.__reviews.append(parser.next_review())
@@ -17,7 +18,7 @@ class FileData:
         return str(self.__reviews)
 
     def __read_from_file(self, path):
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding=self.__encoding) as f:
             for line in f:
                 if line.startswith('score'):
                     score = float(line.split()[1])
@@ -26,12 +27,11 @@ class FileData:
                     self.__reviews.append(ReviewData(score, text))
 
     def write_to_file(self, filename):
-        if self.__parser is not None:
-            with open(filename, 'w', encoding=self.__parser.get_encoding()) as f:
-                f.writelines([str(review) for review in self.__reviews])
-        else:
-            with open(filename, 'w') as f:
-                f.writelines([str(review) for review in self.__reviews])
+        encoding = self.__parser.get_encoding() if self.__parser is not None else self.__encoding
+
+        with open(filename, 'w', encoding=encoding) as f:
+            f.write('score, text\n')
+            f.writelines([str(review) for review in self.__reviews])
 
 
 class ReviewData:
@@ -47,7 +47,7 @@ class ReviewData:
         return 'score: {0}\ntext:  {1}\n'.format(self.__score, self.__text)
 
     def __str__(self):
-        return 'score: {0}\ntext:  {1}\n'.format(self.__score, self.__text)
+        return '{0}, {1}'.format(self.__score, self.__text)
 
     def get_score(self):
         return self.__score
--- dataParsing/data_organizer.py
+++ dataParsing/data_organizer.py
@@ -1,6 +1,6 @@
+import os
 from dataParsing.Parser import LargeFileParser
 from dataParsing.FileData import *
-
 
 LARGE_INPUT_FILE = '../ml-dataset/movies.txt'
 LARGE_INPUT_FILE_ENCODING = 'iso-8859-1'
@@ -22,11 +22,19 @@ def parse_file(path):
         remaining = length - parser.get_line_cursor()
 
     for i, data in enumerate(file_data):
-        data.write_to_file('../out/{0}_{1}.txt'.format(path.split('.')[0].split('/')[1], i + 1))
+        data.write_to_file('../csv/{0}_{1}.txt'.format(path.split('.')[0].split('/')[1], i + 1))
+
+
+def convert_to_csv(src, dst):
+    files_data = [FileData(STEP_SIZE, path=os.path.join(src, f), encoding=LARGE_INPUT_FILE_ENCODING) for f
+                  in os.listdir(src)]
+    for i, data in enumerate(files_data):
+        data.write_to_file(os.path.join(dst, 'movies_{0}.txt'.format(i + 1)))
 
 
 def main():
-    parse_file(LARGE_INPUT_FILE)
+    # parse_file(LARGE_INPUT_FILE)
+    convert_to_csv('../out', '../csv')
 
 
 if __name__ == '__main__':
